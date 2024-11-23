d13ff622552b0f25040722ffafd6939ec08a15ac
Dharmik <whatsapp83trending@gmail.com>
2024-11-24 01:06:52 +0530

    cron updated

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use(globalErrorHandler);
 // scrapingService();
 
 // Schedule the task to run at 12 AM New Zealand Time
-cron.schedule('0 10 * * *', scrapingService, {
+cron.schedule('42 8 * * *', scrapingService, {
   scheduled: true,
   timezone: 'Pacific/Auckland', // New Zealand timezone
 });
