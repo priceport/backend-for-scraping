e00e57b2354fa008a2d0afad66f80520843d4f29
Dharmik <whatsapp83trending@gmail.com>
2024-11-24 18:28:40 +0530

    caching added to pricechange

--- routes/pricechange.routes.js
+++ routes/pricechange.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const PricechangeController = require('../controllers/pricechange.controller.js');
 const UserController = require('../controllers/user.controller.js');
+const cacheMiddleware = require('../helpers/cacheMiddleware.js');
 
 router.route("/")
-.get(UserController.isLoggedIn,PricechangeController.priceChangeGraph);
+.get(UserController.isLoggedIn,cacheMiddleware,PricechangeController.priceChangeGraph);
 
 router.route("/product")
-.get(UserController.isLoggedIn,PricechangeController.getLivePriceChanges);
+.get(UserController.isLoggedIn,cacheMiddleware,PricechangeController.getLivePriceChanges);
 module.exports = router;
