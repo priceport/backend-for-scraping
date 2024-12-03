4b25e965ea9370d0e450ea7c825e3acb0967fa7c
Dharmik <whatsapp83trending@gmail.com>
2024-12-03 16:11:44 +0530

    cacheMiddleware updated to only cache 200 response

--- helpers/cacheMiddleware.js
+++ helpers/cacheMiddleware.js
@@ -1,31 +1,34 @@
 const redisClient = require("../configs/redis.config");
 
 const cacheMiddleware = async (req, res, next) => {
-    const cacheKey = req.originalUrl; // Use the request URL as the cache key
-  
-    try {
-      // Check if the response is cached
-      const cachedData = await redisClient.get(cacheKey);
-  
-      if (cachedData) {
-        console.log('Cache hit');
-        return res.status(200).json(JSON.parse(cachedData));
-      }
-  
-      console.log('Cache miss');
-      // Override the `res.send` method to cache the response
-      const originalSend = res.send.bind(res);
-  
-      res.send = (body) => {
-        redisClient.setEx(cacheKey, 86400, body); // Set a 24-hour expiry
-        originalSend(body);
-      };
-  
-      next();
-    } catch (err) {
-      console.error('Redis error:', err);
-      next(); // Continue without caching if Redis fails
+  const cacheKey = req.originalUrl; // Use the request URL as the cache key
+
+  try {
+    // Check if the response is cached
+    const cachedData = await redisClient.get(cacheKey);
+
+    if (cachedData) {
+      console.log('Cache hit');
+      return res.status(200).json(JSON.parse(cachedData));
     }
-  };
+
+    console.log('Cache miss');
+    // Override the `res.send` method to cache the response
+    const originalSend = res.send.bind(res);
+
+    res.send = (body) => {
+      // Only cache successful responses (status code 200)
+      if (res.statusCode === 200) {
+        redisClient.setEx(cacheKey, 86400, body); // Set a 24-hour expiry
+      }
+      originalSend(body);
+    };
+
+    next();
+  } catch (err) {
+    console.error('Redis error:', err);
+    next(); // Continue without caching if Redis fails
+  }
+};
 
 module.exports = cacheMiddleware;
