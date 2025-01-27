e92d6dbb3395e58bf5406b6d88fb869cad42561d
Dharmik <whatsapp83trending@gmail.com>
2025-01-27 22:05:06 +0530

    get all orgs protected

--- routes/organisation.routes.js
+++ routes/organisation.routes.js
@@ -5,6 +5,6 @@ const UserController = require('../controllers/user.controller.js');
 const router = express.Router();
 
 router.route("/")
-.get(UserController?.isLoggedIn,OrganisationController?.getAllOrganisations);
+.get(UserController?.isLoggedIn,UserController.isAuthorized('admin'),OrganisationController?.getAllOrganisations);
 
 module.exports = router;
