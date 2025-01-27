a9e063e68d24ba7b8ca517e6338db9972febc0a9
Dharmik <whatsapp83trending@gmail.com>
2025-01-27 21:22:58 +0530

    Get all user, update user and delete user done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,14 @@ const UserController = require('../controllers/user.controller.js');
 const router = express.Router();
 
 //user routes
-router.route("/").get(UserController.isLoggedIn,UserController.getUserProfile);
+router.route("/")
+.get(UserController.isLoggedIn,UserController.getUserProfile)
+.put(UserController.isLoggedIn,UserController.isAuthorized('admin'),UserController.updateUser);
+
+router.route("/:id")
+.delete(UserController.isLoggedIn,UserController.isAuthorized('admin'),UserController.deleteUser)
+
+router.route("/all").get(UserController.isLoggedIn,UserController.isAuthorized("admin"),UserController.getAllUsers);
 router.route("/register").post(UserController.register);
 router.route("/login").post(UserController.login);
 router.route("/otp/email").get(UserController.sendEmailOtp);
