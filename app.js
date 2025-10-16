c645b8a797fea7f095dd35cae1eeb3218bff0fe1
Dharmik <whatsapp83trending@gmail.com>
2025-10-16 11:24:37 +0530

    scrapingservice run

--- app.js
+++ app.js
@@ -753,7 +753,7 @@ async function runPrecomputes(){
 }
 
 runPrecomputes();
-
+scrapingService();
 
 // Schedule the task to run at 12 AM New Zealand Time
 cron.schedule('50 7 * * *', scrapingService, {
