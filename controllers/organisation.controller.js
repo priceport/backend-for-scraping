2c88575f84b7227c86fe70432a798fe782a08f79
dharmik-bhadra <dharmikbhadra8@gmail.com>
2025-11-27 14:47:01 +0530

    remove org id api

--- controllers/organisation.controller.js
+++ controllers/organisation.controller.js
@@ -2,29 +2,12 @@ const pool = require("../configs/postgresql.config");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-exports.getAllOrganisations = catchAsync(async (req,res,next)=>{
-
-    const orgId = req.params.orgId;
-
-    if(!orgId){
-        return next(new AppError("Organisation ID is required", 400));
-    }
-
-    const organisation = await pool.query(`SELECT * from organisation where id = $1;`,[orgId]);
-
-    if(organisation.rows.length === 0){
-        return next(new AppError("Organisation not found", 404));
-    }
-
-    const authorizedOrgIds = [1, 2]; 
-    
-    if (!authorizedOrgIds.includes(parseInt(orgId))){
-        return next(new AppError("You are not authorized to access this resource", 403));
-    }
+exports.getOrganisationsList = catchAsync(async (req,res,next)=>{
+    const organisations = await pool.query(`SELECT * from organisation ORDER BY id ASC;`);
 
     return res.status(200).json({
         status:"success",
-        message:"User fetched succesfully",
-        data: organisation.rows[0]
+        message:"Organisations fetched successfully",
+        data: organisations.rows
     });
 })
